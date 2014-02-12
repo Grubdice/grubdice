afc6feb2a6ce48bc311a53ccdf72e2db78d028a8
Ethan Hall <ethan@ehdev.io>
2014-02-11 22:33:12 -0600

    adding tests

--- src/main/groovy/co/grubdice/scorekeeper/engine/LudicrousScoreEngineImpl.groovy
+++ src/main/groovy/co/grubdice/scorekeeper/engine/LudicrousScoreEngineImpl.groovy
@@ -1,35 +1,10 @@
 package co.grubdice.scorekeeper.engine
 
-import co.grubdice.scorekeeper.dao.helper.PlayerDaoHelper
-import co.grubdice.scorekeeper.model.external.ScoreResult
 import co.grubdice.scorekeeper.model.persistant.GameType
-import co.grubdice.scorekeeper.model.persistant.Season
-import com.google.common.annotations.VisibleForTesting
 import org.springframework.stereotype.Repository
-
-import javax.transaction.Transactional
 
 @Repository
 class LudicrousScoreEngineImpl extends CommonScoreEngineImpl implements LudicrousScoreEngine {
-
-    @Transactional
-    void updateSeasonScores(List<ScoreResult> results, Season season) {
-        results.first().name.each { name ->
-            updateScoreForWinner(name, results.size(), season)
-        }
-    }
-
-    @VisibleForTesting
-    void updateScoreForWinner(String name, int numberOfPlayers, Season season) {
-        def player = PlayerDaoHelper.verifyPlayerExists(playerDao, name)
-        def seasonScore = getSeasonScoreForPlayer(player, season)
-        seasonScore.currentScore += getScore(numberOfPlayers)
-        seasonScoreDao.save(seasonScore)
-    }
-
-    def Integer getScore(int numberOfPlayers){
-        return getScore(numberOfPlayers - 1, 0)
-    }
 
     def Integer getScore(int numberOfPlayersWonTo, int numberOfPlayersLostTo) {
         if(numberOfPlayersLostTo == 0) {
--- src/test/groovy/co/grubdice/scorekeeper/engine/LeagueScoreEngineImplTest.groovy
+++ src/test/groovy/co/grubdice/scorekeeper/engine/LeagueScoreEngineImplTest.groovy
@@ -1,51 +1,16 @@
 package co.grubdice.scorekeeper.engine
-import co.grubdice.scorekeeper.dao.PlayerDao
-import co.grubdice.scorekeeper.dao.SeasonScoreDao
-import co.grubdice.scorekeeper.model.external.ScoreResult
-import co.grubdice.scorekeeper.model.persistant.Player
-import co.grubdice.scorekeeper.model.persistant.Season
-import co.grubdice.scorekeeper.model.persistant.SeasonScore
-import org.testng.annotations.BeforeMethod
+
 import org.testng.annotations.Test
 
 import static org.fest.assertions.Assertions.assertThat
 
 class LeagueScoreEngineImplTest {
 
-    LeagueScoreEngineImpl leagueScoreEngine
-
-    @BeforeMethod
-    public void setup() {
-        leagueScoreEngine = new LeagueScoreEngineImpl()
-    }
-
     @Test
-    public void testGetScore() throws Exception {
-        assertThat(leagueScoreEngine.getScore(0, [1,1,1])).isEqualTo(2)
-        assertThat(leagueScoreEngine.getScore(1, [1,1,1])).isEqualTo(0)
-        assertThat(leagueScoreEngine.getScore(2, [1,1,1])).isEqualTo(-2)
-
-        assertThat(leagueScoreEngine.getScore(0, [1,1,1,1,1,1])).isEqualTo(5)
-        assertThat(leagueScoreEngine.getScore(1, [1,1,1,1,1,1])).isEqualTo(3)
-        assertThat(leagueScoreEngine.getScore(2, [1,1,1,1,1,1])).isEqualTo(1)
-        assertThat(leagueScoreEngine.getScore(3, [1,1,1,1,1,1])).isEqualTo(-1)
-        assertThat(leagueScoreEngine.getScore(4, [1,1,1,1,1,1])).isEqualTo(-3)
-        assertThat(leagueScoreEngine.getScore(5, [1,1,1,1,1,1])).isEqualTo(-5)
-
-        assertThat(leagueScoreEngine.getScore(0, [1,2])).isEqualTo(2)
-        assertThat(leagueScoreEngine.getScore(1, [1,2])).isEqualTo(-1)
-    }
-
-    @Test
-    public void testCalculatingScore() throws Exception {
-        def playerDao = [ findByNameLikeIgnoreCase: {String name -> return new Player(name)} ] as PlayerDao
-        def seasonScoreDao = [ save: { SeasonScore seasonScore ->
-                if(seasonScore.player.name == 'player1') { assertThat(seasonScore.currentScore).isEqualTo(2)}
-                if(seasonScore.player.name == 'player2') { assertThat(seasonScore.currentScore).isEqualTo(0)}
-            },
-            findByPlayerAndSeason: {Player p, Season s -> return new SeasonScore(s, p, 1)} ] as SeasonScoreDao
-        leagueScoreEngine.setPlayerDao(playerDao)
-        leagueScoreEngine.setSeasonScoreDao(seasonScoreDao)
-        leagueScoreEngine.updateSeasonScores([new ScoreResult(["player1"]), new ScoreResult(["player2"])], new Season())
+    public void testScoreCalculation() throws Exception {
+        LeagueScoreEngineImpl leagueScoreEngine = new LeagueScoreEngineImpl()
+        assertThat(leagueScoreEngine.getScore(4, 2)).isEqualTo(2)
+        assertThat(leagueScoreEngine.getScore(1, 2)).isEqualTo(-1)
+        assertThat(leagueScoreEngine.getScore(5, 0)).isEqualTo(5)
     }
 }
