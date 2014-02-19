d9db44d45664eef8860c54c68876452bacafef49
Ethan Hall <ethan@ehdev.io>
2014-02-18 21:27:20 -0600

    updaing logger

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -3,6 +3,8 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.rolling.RollingFileAppender
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+import ch.qos.logback.classic.PatternLayout
+import com.logentries.logback.LogentriesAppender
 
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
@@ -10,10 +12,19 @@ appender("STDOUT", ConsoleAppender) {
     }
 }
 
+appender("LE", LogentriesAppender) {
+  token = "a00f98cc-5fa8-4cdc-94e7-8ca97afdbe94"
+  ssl = "False"
+  facility = "USER"
+  layout(PatternLayout) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+  }
+}
+
 appender("ROOT-LOG", RollingFileAppender) {
     file = System.getProperty("catalina.base") + "/logs/grub-dice.log"
     rollingPolicy(TimeBasedRollingPolicy) {
-        fileNamePattern = "logFile.%d{yyyy-MM-dd}.log"
+        fileNamePattern = "grubdice.%d{yyyy-MM-dd}.log"
         maxHistory = 30
     }
     encoder(PatternLayoutEncoder) {
@@ -24,7 +35,7 @@ appender("ROOT-LOG", RollingFileAppender) {
 logger("org.hibernate", Level.WARN)
 
 if(System.getProperty("spring.profiles.active")?.equalsIgnoreCase("prod")){
-    root(Level.INFO, ["STDOUT", "ROOT-LOG"])
+    root(Level.INFO, ["STDOUT", "ROOT-LOG", "LE"])
 } else {
     root(Level.DEBUG, ["STDOUT"])
 }
